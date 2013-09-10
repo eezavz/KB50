eb84f9c6cb55f21b62f2d4ad2d15c7c2faf26d17
Furkan <eezavz@outlook.com>
2013-09-10 00:37:15 -0700

    Project aangemaakt.

---
+++ Damspel/Damspel/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Damspel
+//
+//  Created by Furkan on 9/10/13.
+//  Copyright (c) 2013 Groep 1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
